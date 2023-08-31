30708020be95e60b4e49ac54e1f872e8a0739cd6
nanduna <nandunabekoon@gmail.com>
2023-08-31 20:19:01 +0530

    First Commit

---
+++ Pill_Reminder_App/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {}
+
+class PillRemainderApp extends StatelessWidget {
+  const PillRemainderApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
